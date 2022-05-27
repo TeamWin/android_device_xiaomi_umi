84a1e05f80aa8ba1facfd889c48c3ed3a45048cf
me-cafebabe <me_cafebabe@hotmail.com>
2022-05-27 16:24:19 +0800

    umi: Copy vendor blobs from vendor tree

--- device.mk
+++ device.mk
@@ -25,3 +25,9 @@ $(call inherit-product, $(COMMON_PATH)/common.mk)
 
 # Copy modules for depmod
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.ko,$(DEVICE_PATH)/prebuilt,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1)
+
+# Vendor blobs
+ifneq ($(wildcard vendor/xiaomi/umi/proprietary/),)
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/umi/proprietary/,$(TARGET_COPY_OUT_RECOVERY)/root/)
+endif
