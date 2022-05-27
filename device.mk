e98bab563ef077811d17bc6d6a5014a29cbd5eba
me-cafebabe <me_cafebabe@hotmail.com>
2022-05-27 16:32:24 +0800

    umi: Drop kernel modules

--- device.mk
+++ device.mk
@@ -23,9 +23,6 @@
 # Inherit from OEM SOC-common
 $(call inherit-product, $(COMMON_PATH)/common.mk)
 
-# Copy modules for depmod
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.ko,$(DEVICE_PATH)/prebuilt,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1)
-
 # Vendor blobs
 ifneq ($(wildcard vendor/xiaomi/umi/proprietary/),)
 PRODUCT_COPY_FILES += \
